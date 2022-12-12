4db29654b17feb31c1ed0714bb8f706d5f39a88c
Dennis ten Hoove <dennistenhoove@protonmail.com>
2022-12-12 21:55:28 +0100

    Update filepaths

--- flatpak-init.sh
+++ flatpak-init.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ -f "/var/lib/arkane/flatpak-installation" ]];
+if [[ -f "/var/lib/arkane/flatpak-init.info" ]];
 then
 	notify-send -a "flatpak-init" -u normal -w \
 		"Installation of core flatpak applications is ongoing" \
 		"The installation of core applications is still ongoing, please allow some time for the process to complete"	
 else
-	echo "/var/lib/arkane/flatpak-installing does not exist, assuming no ongoing installation"
+	echo "/var/lib/arkane/flatpak-init.info does not exist, assuming no ongoing installation"
 fi
 
 exit 0
