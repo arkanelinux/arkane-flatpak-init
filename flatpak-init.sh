03faa2f419350b64bcd46d0e88501d83643e09d0
Dennis ten Hoove <dennistenhoove@protonmail.com>
2022-12-14 18:45:16 +0100

    Also inform user about installation completion

--- flatpak-init.sh
+++ flatpak-init.sh
@@ -2,11 +2,22 @@
 
 if [[ -f "/var/lib/arkane/flatpak-init.info" ]];
 then
-	notify-send -a "flatpak-init" -u normal -w \
+	notify-send -a "flatpak-init" -i /usr/share/pixmaps/arkane-logo.png -u critical -w \
 		"Installation of core flatpak applications is ongoing" \
-		"The installation of core applications is still ongoing, please allow some time for the process to complete"	
+		"The installation of core applications is still ongoing, please allow some time for the process to complete" &
+
+	while [[ ! -f /var/lib/arkane/flatpak-init.lock ]]
+	do 
+		sleep 2
+	done
+
+	notify-send -a "flatpak-init" -i /usr/share/pixmaps/arkane-logo.png -u normal -w \
+		"Installation of core flatpak applications has finished" \
+		"The installation of core applications has just finished, all core applications have now been installed"	
+
+	exit 0
 else
 	echo "/var/lib/arkane/flatpak-init.info does not exist, assuming no ongoing installation"
+	
+	exit 0
 fi
-
-exit 0
