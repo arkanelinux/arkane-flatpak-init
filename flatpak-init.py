21c142232e82b9b688898b4100b96e1f8a7a20fd
Dennis ten Hoove <dennistenhoove@protonmail.com>
2024-02-06 23:58:50 +0100

    Improve scripts

--- flatpak-init.py
+++ flatpak-init.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 import gi, os
 
-gi.require_version('Flatpak', '1.0')
+gi.require_version("Flatpak", "1.0")
 from gi.repository import Flatpak
 
 # As recommended by Flatpak maintainers, it is best to not bash script Flatpak
 # thus we are using the Flatpak API
 
 # Write file to trigger autostart script which informs user about ongoing installation
+if not os.path.exists("/var/lib/arkane"):
+    os.mkdir("/var/lib/arkane")
 open("/var/lib/arkane/flatpak-init.info", "w")
 
 installation = Flatpak.Installation.new_system()
 transaction = Flatpak.Transaction.new_for_installation(installation, None)
-remote = "flathub"
 
-with open('/etc/arkane/flatpak-init/flatpak.list') as flatpak_list:
+with open("/etc/arkane/flatpak-init/flatpak.list") as flatpak_list:
     for i, ref in enumerate(flatpak_list):
         print("Adding " + ref)
-        transaction.add_install(remote, ref.strip(), None)
+        transaction.add_install("flathub", ref.strip(), None)
 
 transaction.run(None)
 
--- flatpak-init.sh
+++ flatpak-init.sh
@@ -1,25 +1,39 @@
 #!/usr/bin/env bash
+#
 
-# Install flatpaks and notify
+# Do not yet do anything as long as the service has not yet started
+while [[ ! -f /var/lib/arkane/flatpak-init.lock ]] && [[ ! -f /var/lib/arkane/flatpak-init.info ]]; do
+	sleep 5
+
+	if [[ -z $has_notified ]]; then
+		# Notify user about internet requirement
+		notify-send -a 'flatpak-init' -i /usr/share/pixmaps/arkanelinux-logo.png -u critical -w \
+			'Installation of core Flatpak apps' \
+			'Once a network connection has been established the installation of core applications will begin' &
+
+		has_notified=1
+	fi
+done
+
+# If info file exists we are running the service
 if [[ -f '/var/lib/arkane/flatpak-init.info' ]]; then
 
+	# Notify user about ongoing installation
 	notify-send -a 'flatpak-init' -i /usr/share/pixmaps/arkanelinux-logo.png -u critical -w \
-		'Installation of core flatpak applications is ongoing' \
+		'Installation of core Flatpak apps is ongoing' \
 		'The installation of core applications is still ongoing, please allow some time for the process to complete' &
 
+	# Wait untill install finished
 	while [[ ! -f /var/lib/arkane/flatpak-init.lock ]]; do
-		sleep 2
+		sleep 5
 	done
 
+	# Notify user install finished
 	notify-send -a 'flatpak-init' -i /usr/share/pixmaps/arkanelinux-logo.png -u normal -w \
-		'Installation of core flatpak applications has finished' \
+		'Installation of core Flatpak apps finished' \
 		'The installation of core applications has just finished, all core applications have now been installed'
 
-	exit 0
-
-else
-
-	echo '/var/lib/arkane/flatpak-init.info does not exist, assuming no ongoing installation'
-	exit 0
 
 fi
+
+exit 0
